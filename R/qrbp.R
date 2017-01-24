f05f02434b361f47dbcd0a8e373d2bffda1a7f9a
skiptoniam <skiptoniam@gmail.com>
2017-01-25 10:57:50 +1100

    Setting up r package framework

---
+++ R/qrbp.R
@@ -0,0 +1,11 @@
+#' @title qrbp package
+#' @name qrbp-package
+#' @description qrbp is a package to create quasi-random background points for use in
+#'   Poisson point process modelling in R. The main function is \code{qrbp}, which takes some
+#'   coordinates, an area of interest and covariates and produces a \code{qrbp}
+#'   dataset.
+#' @docType package
+#' @import raster
+#' @import sp
+
+NULL
