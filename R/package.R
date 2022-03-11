87043d33bfed961ca842786e137251fff8048224
skiptoniam <skiptoniam@gmail.com>
2022-03-11 22:47:13 +1100

    package description

---
+++ R/package.R
@@ -0,0 +1,12 @@
+#' package file
+#'
+#' @name ppmData
+#' @description ppmData is a package for setting up quadrature to implement in Poisson Point process models and extensions.
+#'
+#' @docType package
+NULL
+
+# #' @useDynLib ppmData, .registration = TRUE
+# #'
+# #'
+# NULL
