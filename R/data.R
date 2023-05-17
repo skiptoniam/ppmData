eaaadc2a9a9ff98ff9a415a232753a20e92c3f95
Skip Woolley <skip.woolley@csiro.au>
2023-05-17 14:52:57 +1000

    cran-updates

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' Presence only observations of snails from the family Rhytididae located in Tasmania, Australia.
 #'
-#' @source Atlas of Living Australia. \url{http://www.ala.org/}
+#' @source Atlas of Living Australia. \url{https://www.ala.org/}
 #' @format A data frame with columns:
 #' \describe{
 #'  \item{X}{Longitude.}
